2ce5bb4e9c63df872912143205242157c5394be9
Artem Polikov <artem4250@gmail.com>
2021-09-18 01:40:37 +0300

    updated

--- main.c
+++ main.c
@@ -2,53 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define false 0
-#define true 1
-/*
-void CleanLine(char str[])
+struct observatory
 {
-    size_t correct_pointer=0, old_pointer=0;
-    _Bool after_space = false;
-    while(str[old_pointer]==' ') old_pointer++;
-    while(str[old_pointer])
+    size_t id; //KEY
+    char name[64];
+    float latitude; //широта (-90 south; 90 north)
+    float longitude; //долгота (-180 west; 180 east)
+
+    unsigned first_telescope;
+};
+
+struct index_structure
+{
+    size_t index;
+    size_t id;
+
+    _Bool is_removed;
+};
+
+struct telescope
+{
+    size_t id; //KEY
+    char name[64];
+
+    size_t next_telescope;
+};
+
+int
+
+int DB_Terminal()
+{
+    printf("\nNow you are in the Database terminal.\nEnter \"db: help\" for a list of commands.\n\n");
+    char s1[128], s2[128], s3[128], s4[128], s5[128], s6[128];
+    int buff;
+    while(1)
     {
-        if(str[old_pointer]==' ' && !after_space)
+        printf("db: ");
+        scanf("%s %s %s %s %s", s1, s2, s3, s4, s5);
+        strlwr(s1); strlwr(s2); strlwr(s3); strlwr(s4); strlwr(s5);
+        while ((buff = getchar()) != '\n' && buff != EOF) { }
+        if(!strcmp(s1, "help"))
         {
-            after_space = true;
+            printf("\nHELP\n");
         }
-        else if(str[old_pointer]==' ' && after_space)
+        else if(!strcmp(s1, "close"))
         {
-            old_pointer++;
-            continue;
+            return 0;
         }
-        str[correct_pointer] = str[old_pointer];
-        old_pointer++;
-        correct_pointer++;
     }
-    str[correct_pointer]='\0';
-    strlwr(str);
-}*/
+}
+
 
 int main()
 {
-    char buff1[256],buff2[256],buff3[256];
-    while(true)
-    {
-        printf("Enter a string: ");
-        scanf("%s %s %s", buff1, buff2, buff3);
-        printf("---\n%s\n%s\n%s\n---", buff1, buff2, buff3);
-        /*CleanLine(buff);
-        printf("%s\n", buff);
-        if(strstr(buff, "db")==&buff[0])
-        {
-            printf("This is Database!");
-            if(strstr(buff, "exit")==&buff[2])
-            {
-                printf("This is Exit!");
-                break;
-            }
-        }*/
-    }
+    DB_Terminal();
     return 0;
 }
 
